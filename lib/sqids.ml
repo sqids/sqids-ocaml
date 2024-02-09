e5f1f751609c7a5454afd2c74a3ef9785b443fc8
Leo Soares <leojrfs@gmail.com>
2024-02-09 15:55:08 +0000

    lib: add initial sqids ocaml implementation

---
+++ lib/sqids.ml
@@ -0,0 +1,262 @@
+exception Alphabet_contains_multibyte_characters of string
+exception Alphabet_too_short of string
+exception Alphabet_contains_repeated_characters of string
+exception Minimum_length_outside_limits of string
+exception Encode_max_attempts of string
+
+module String_set = Sqids_utils.String_set
+
+type t = { alphabet : Bytes.t; min_length : int; blocklist : String_set.t }
+
+module Defaults = struct
+  let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  let min_length = 0
+  let blocklist = Blocklist.blocklist
+end
+
+(* TODO: optimize *)
+let to_id num alphabet =
+  let alphabet_len = Bytes.length alphabet in
+  let rec loop result id_chars =
+    let alphabet_idx = result mod alphabet_len in
+    (* from the alphabet string get the char in the position with index from the result of the previous operation *)
+    let alphabet_char = Bytes.get alphabet alphabet_idx in
+    (* prepend the previous char to the id_chars string *)
+    let id_chars' = Bytes.cat (Bytes.make 1 alphabet_char) id_chars in
+    let result' = result / alphabet_len in
+    (* break loop if result' is 0 *)
+    if Int.equal result' 0 then id_chars' else loop result' id_chars'
+  in
+  loop num Bytes.empty
+
+let to_number id alphabet =
+  let id_len = String.length id in
+  let alphabet_len = String.length alphabet in
+  let num = ref 0 in
+  (* sum all the values of the list *)
+  for c = 0 to id_len - 1 do
+    let idx = String.index alphabet (String.get id c) in
+    num := (!num * alphabet_len) + idx
+  done;
+  !num
+
+let is_blocked_id t ~id =
+  let id = String.lowercase_ascii id in
+  let id_len = String.length id in
+  let exception Blocked_word in
+  try
+    String_set.iter
+      (fun word ->
+        let word_len = String.length word in
+        if word_len <= id_len then
+          if id_len <= 3 || word_len <= 3 then (
+            if String.equal id word then raise_notrace Blocked_word
+          )
+          else if String.for_all Sqids_utils.char_is_digit id then (
+            if String.starts_with ~prefix:word id || String.ends_with ~suffix:word id then
+              raise_notrace Blocked_word
+          )
+          else if Sqids_utils.string_is_infix ~affix:word id then raise_notrace Blocked_word
+      )
+      t.blocklist;
+    false
+  with Blocked_word -> true
+
+let make ?(alphabet = Defaults.alphabet) ?(min_length = Defaults.min_length)
+    ?(blocklist = Defaults.blocklist) () =
+  if String.length alphabet < 3 then raise (Alphabet_too_short "Alphabet length must be at least 3");
+
+  if Sqids_utils.string_has_dups alphabet then
+    raise (Alphabet_contains_repeated_characters "Alphabet must contain unique characters");
+
+  (* min_length should be equivalent to uint8_t (0-255) *)
+  (* https://github.com/sqids/sqids-spec/blob/40f407169fa0f555b93a197ff0a9e974efa9fba6/src/index.ts#L44 *)
+  let min_length_limit = 255 in
+  if min_length < 0 || min_length > min_length_limit then
+    raise
+      (Minimum_length_outside_limits
+         (Printf.sprintf "Minimum length has to be between 0 and %d" min_length_limit)
+      );
+
+  String.to_seq alphabet
+  |> Seq.iter (fun char ->
+         if Char.code char > 127 then
+           raise
+             (Alphabet_contains_multibyte_characters "Alphabet cannot contain multibyte characters")
+     );
+
+  (* Convert blocklist to String_set *)
+  let blocklist_set = List.to_seq blocklist |> String_set.of_seq in
+
+  (* filter using blocklist *)
+  let filtered_blocklist =
+    let alphabet_lower = String.lowercase_ascii alphabet in
+    String_set.filter_map
+      (fun word ->
+        let word = String.lowercase_ascii word in
+        (* Drop word if characters in word are not in the alphabet, and
+           words with less than 3 chars should be dropped *)
+        if String.length word >= 3 && String.for_all (String.contains alphabet_lower) word then
+          Some word
+        else None
+      )
+      blocklist_set
+  in
+
+  let alphabet = Bytes.of_string alphabet in
+  Sqids_utils.bytes_shuffle_inplace alphabet;
+
+  { alphabet; min_length; blocklist = filtered_blocklist }
+
+let rec encode_numbers t ~numbers ?(increment = 0) () =
+  let alphabet = t.alphabet in
+
+  let alphabet_len = Bytes.length alphabet in
+  let numbers_len = List.length numbers in
+
+  (* Fail when an increment-number of attempts has been made to re-generated the ID,
+     where increment is alphabet length + 1 *)
+  if increment > alphabet_len then
+    raise (Encode_max_attempts "Reached max attempts to re-generate the ID");
+
+  (* get a semi-random offset from input numbers *)
+  let offset =
+    List.fold_left
+      (fun a (idx, v) ->
+        (* Calculate the new value by taking the remainder when dividing by the length of 'alphabet' *)
+        let alphabet_idx = v mod alphabet_len in
+        (* Add the index 'i' and the accumulated value 'a' *)
+        let alphabet_v = Bytes.get_uint8 alphabet alphabet_idx in
+        alphabet_v + idx + a
+      )
+      (* Start with the size of the numbers list' *)
+      numbers_len
+      (* Enumerate over the elements of 'numbers' along with their indices *)
+      (List.mapi (fun i v -> (i, v)) numbers)
+  in
+
+  (* if there is a non-zero `increment`, it's an internal attempt to re-generated the ID *)
+  let offset = (offset + increment) mod alphabet_len in
+
+  (* re-arrange alphabet so that second-half goes in front of the first-half *)
+  let alphabet = Sqids_utils.bytes_rotate alphabet offset in
+
+  (* `prefix` is the first character in the generated ID, used for randomization *)
+  let prefix = Bytes.get alphabet 0 in
+
+  (* reverse alphabet (otherwise for [0, x] `offset` and `separator` will be the same char) *)
+  Sqids_utils.bytes_rev_inplace alphabet;
+
+  (* final ID will always have the `prefix` character at the beginning *)
+  (* create buffer for the resulting id with an arbitrary initial size of 4 bytes *)
+  let id_buf = Buffer.create 4 in
+  Buffer.add_char id_buf prefix;
+
+  (* encode input array *)
+  List.iteri
+    (fun idx num ->
+      (* the first character of the alphabet is going to be reserved for the `separator` *)
+      let alphabet_no_sep = Bytes.sub alphabet 1 (Bytes.length alphabet - 1) in
+      Buffer.add_bytes id_buf (to_id num alphabet_no_sep);
+
+      (* if not the last number *)
+      if idx < numbers_len - 1 then (
+        (* separator` character is used to isolate numbers within the ID *)
+        Buffer.add_bytes id_buf (Bytes.sub alphabet 0 1);
+
+        (* shuffle on every iteration *)
+        Sqids_utils.bytes_shuffle_inplace alphabet
+      )
+    )
+    numbers;
+
+  (* handle `minLength` requirement, if the ID is too short *)
+  if t.min_length > Buffer.length id_buf then (
+    (* append a separator *)
+    Buffer.add_bytes id_buf (Bytes.sub alphabet 0 1);
+
+    (* keep appending `separator` + however much alphabet is needed
+       for decoding: two separators next to each other is what tells us
+       the rest are junk characters *)
+    while t.min_length - Buffer.length id_buf > 0 do
+      Sqids_utils.bytes_shuffle_inplace alphabet;
+      let slice_len = min (t.min_length - Buffer.length id_buf) (Bytes.length alphabet) in
+      Buffer.add_subbytes id_buf alphabet 0 slice_len
+    done
+  );
+
+  let id = Buffer.contents id_buf in
+
+  (* if ID has a blocked word anywhere, restart with a +1 increment *)
+  match is_blocked_id t ~id with
+  | true -> encode_numbers t ~numbers ~increment:(increment + 1) ()
+  | false -> id
+
+let encode t (numbers : int list) : string =
+  match numbers with
+  (* if no numbers passed, return an empty string *)
+  | [] -> ""
+  (* don't allow out-of-range numbers *)
+  | numbers ->
+      if List.exists (fun nr -> nr < 0) numbers then
+        raise
+          (Invalid_argument
+             (Printf.sprintf "Encoding supports numbers between 0 and %d"
+                max_int));
+      encode_numbers t ~numbers ()
+
+let decode t id =
+  (* check if id is an empty string, if so, return an empty array *)
+  match id with
+  | "" -> []
+  | id -> (
+    (* check if chars in id are in the alphabet, if not, return the empty array *)
+    match String.for_all (Bytes.contains t.alphabet) id with
+    | false -> []
+    | true ->
+      let alphabet = t.alphabet in
+
+      (* first character is always the `prefix` *)
+      let prefix = String.get id 0 in
+
+      (* `offset` is the semi-random position that was generated during encoding *)
+      let offset = Bytes.index alphabet prefix in
+
+      (* re-arrange alphabet back into it's original form *)
+      let alphabet = Sqids_utils.bytes_rotate alphabet offset in
+
+      (* reverse alphabet *)
+      Sqids_utils.bytes_rev_inplace alphabet;
+
+      (* now it's safe to remove the prefix character from ID, it's not needed anymore *)
+      let id = String.sub id 1 (String.length id - 1) in
+
+      let rec process_id id' numbers =
+        match String.length id' with
+        (* stop when `id` length is 0 and return numbers *)
+        | 0 -> numbers
+        | _ -> (
+          let separator = Bytes.get alphabet 0 in
+          (* we need the first part to the left of the separator to decode the number *)
+          let chunks = String.split_on_char separator id' in
+          match chunks with
+          | [] -> numbers
+          (* only run this if `chunks` length is not 0 *)
+          | hd :: tl ->
+            (* if chunk[0] is empty, we are done (the rest are junk characters) *)
+            if hd = "" then numbers
+            else
+              (* decode the number without using the `separator` character *)
+              let alphabet_without_separator = Bytes.sub alphabet 1 (Bytes.length alphabet - 1) in
+              let decoded_number = to_number hd (alphabet_without_separator |> String.of_bytes) in
+              let numbers' = List.append numbers [ decoded_number ] in
+              (* if this ID has multiple numbers, shuffle the alphabet because that's what encoding function did *)
+              if List.length chunks > 1 then (
+                Sqids_utils.bytes_shuffle_inplace alphabet;
+                process_id (String.concat (Char.escaped separator) tl) numbers'
+              )
+              else numbers'
+        )
+      in
+      process_id id []
+  )
---
+++ lib/sqids_utils.ml
@@ -0,0 +1,67 @@
+module String_set = Set.Make (String)
+module Char_set = Set.Make (Char)
+
+let bytes_swap_inplace bytes i j =
+  let tmp = Stdlib.Bytes.get bytes i in
+  Stdlib.Bytes.set bytes i (Stdlib.Bytes.get bytes j);
+  Stdlib.Bytes.set bytes j tmp
+
+let bytes_rev_inplace bytes =
+  let len = Bytes.length bytes in
+  for i = 0 to (len / 2) - 1 do
+    let tmp = Bytes.get bytes i in
+    Bytes.set bytes i (Bytes.get bytes (len - 1 - i));
+    Bytes.set bytes (len - 1 - i) tmp
+  done
+
+let bytes_shuffle_inplace bytes =
+  let bytes_len = Stdlib.Bytes.length bytes in
+  (* ocaml for loop is inclusive, subctract 1 to stop before the last value *)
+  for i = 0 to bytes_len - 1 - 1 do
+    let j = bytes_len - 1 - i in
+    let r =
+      ((i * j) + Stdlib.Bytes.get_uint8 bytes i + Stdlib.Bytes.get_uint8 bytes j) mod bytes_len
+    in
+    bytes_swap_inplace bytes i r
+  done
+
+(* TODO: maybe also do it inplace? *)
+let bytes_rotate bytes offset =
+  let len = Bytes.length bytes in
+  let rotated_bytes = Bytes.create len in
+  for i = 0 to len - 1 do
+    Bytes.set rotated_bytes i (Bytes.get bytes ((i + offset) mod len))
+  done;
+  rotated_bytes
+
+let char_is_digit c =
+  match c with
+  | '0' .. '9' -> true
+  | _ -> false
+
+let string_has_dups str =
+  let module Char_set = Set.Make (Char) in
+  let set = Stdlib.String.fold_left (fun set c -> Char_set.add c set) Char_set.empty str in
+  Stdlib.String.length str <> Char_set.cardinal set
+
+(* Partially adapted from https://github.com/dbuenzli/astring by dbuenzli licensed under ISC. *)
+external length : string -> int = "%string_length"
+external unsafe_get : string -> int -> char = "%string_unsafe_get"
+
+let string_is_infix ~affix s =
+  let len_a = length affix in
+  let len_s = length s in
+  if len_a > len_s then false
+  else
+    let max_idx_a = len_a - 1 in
+    let max_idx_s = len_s - len_a in
+    let rec loop i k =
+      if i > max_idx_s then false
+      else if k > max_idx_a then true
+      else if k > 0 then
+        if unsafe_get affix k = unsafe_get s (i + k) then loop i (k + 1) else loop (i + 1) 0
+      else if unsafe_get affix 0 = unsafe_get s i then loop i 1
+      else loop (i + 1) 0
+    in
+    loop 0 0
+
