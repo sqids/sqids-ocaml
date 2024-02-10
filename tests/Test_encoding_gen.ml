10a6dc8e6222537fcd7232b1f574e6e502e8b60a
Leo Soares <leojrfs@gmail.com>
2024-02-10 16:24:19 +0000

    tests: backport `List.equal`

--- tests/Test_encoding_gen.ml
+++ tests/Test_encoding_gen.ml
@@ -1,9 +1,15 @@
 open QCheck2
 
+let rec list_equal eq l1 l2 =
+  match (l1, l2) with
+  | [], [] -> true
+  | [], _ :: _ | _ :: _, [] -> false
+  | a1 :: l1, a2 :: l2 -> eq a1 a2 && list_equal eq l1 l2
+
 let sqids = Sqids.make ()
 
 let roundtrip nums =
-  List.equal Int.equal (Sqids.decode sqids (Sqids.encode sqids nums)) nums
+  list_equal Int.equal (Sqids.decode sqids (Sqids.encode sqids nums)) nums
 
 let test_max_int =
   Test.make ~name:"roundtrip" ~count:1000
