d8a32417403db8899cf6e942e11d23d42030fa09
Leo Soares <leojrfs@gmail.com>
2024-02-10 17:57:27 +0000

    examples: format code with ocamlformat options

--- examples/example.ml
+++ examples/example.ml
@@ -3,7 +3,8 @@ open Printf
 let sqids = Sqids.make ~min_length:10 ()
 
 let () =
-  let id = Sqids.encode sqids [1; 2; 3] in
+  let id = Sqids.encode sqids [ 1; 2; 3 ] in
   printf "encode [1; 2; 3] = %S\n" id;
   let numbers = Sqids.decode sqids id in
-  printf "decode %S = [%s]\n" id (String.concat "; " (List.map string_of_int numbers))
+  printf "decode %S = [%s]\n" id
+    (String.concat "; " (List.map string_of_int numbers))
